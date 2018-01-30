27dd0c40fd3af8205b4c1f69ef5f0d81ad97936d
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-01-30 09:03:38 -0700

    finished stage 9

--- src/Stage 9/components/EmployeeEditor.js
+++ src/Stage 9/components/EmployeeEditor.js
@@ -2,14 +2,59 @@ import React, { Component } from 'react';
 
 class EmployeeEditor extends Component {
   // constructor
-
+  constructor(props){
+    super(props);
+    this.state = {
+      employee: null,
+      originalEmployee: null,
+      notModified: true,
+    }
+    this.save = this.save.bind(this);
+    this.cancel = this.cancel.bind(this);
+  }
   // componentWillReceiveProps
+  componentWillReceiveProps(props){
+    this.setState({
+      employee: {...props.selected},
+      originalEmployee: props.selected,
+      notModified: true,
+    })
+  }
 
   // handleChange
+  handleChange(prop, val){
+    if(this.state.notModified){
+      this.setState({
+        notModified: false
+      })
+    }
 
+    var employeeCopy = {...this.state.originalEmployee};
+    employeeCopy[prop] = val;
+    this.setState({
+      employee: employeeCopy,
+    })
+
+  }
   // save
+  save(){
+    this.state.originalEmployee.updateName(this.state.employee.name);
+    this.state.originalEmployee.updatePhone(this.state.employee.phone);
+    this.state.originalEmployee.updateTitle(this.state.employee.title);
+    this.setState({
+      notModified: true,
+    })
+    this.props.refreshList();
+  }
 
   // cancel
+  cancel(){
+    this.setState({
+      employee: {...this.state.originalEmployee},
+      originalEmployee: this.state.originalEmployee,
+      notModified: true,
+    })
+  }
   
   render() {
     return (
